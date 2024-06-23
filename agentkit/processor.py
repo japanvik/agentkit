0caef9c4950609dfd26a60370805c44f05de29d9
Vik Kumar <vik@japanvik.net>
2024-06-23 07:39:09 -0700

    Removed Emojis which got in the way of Japanese text generation

--- agentkit/processor.py
+++ agentkit/processor.py
@@ -1,8 +1,7 @@
 import json
 import re
 
-from litellm import acompletion, set_verbose
-
+from litellm import acompletion
 
 class JSONParseError(ValueError):
     """
@@ -50,6 +49,7 @@ async def llm_processor(llm_model: str,
         api_base=api_base,
         stop=stop
     )
+    acompletion()
 
     return response.choices[0].message.content.strip()
 
@@ -100,23 +100,14 @@ def remove_emojis(data):
         str: The text string without emojis.
     """
 
+
     emoj = re.compile("["
-        u"\U0001F600-\U0001F64F"  # emoticons
-        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-        u"\U0001F680-\U0001F6FF"  # transport & map symbols
-        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-        u"\U00002500-\U00002BEF"  # chinese char
-        u"\U00002702-\U000027B0"
-        u"\U000024C2-\U0001F251"
-        u"\U0001f926-\U0001f937"
-        u"\U00010000-\U0010ffff"
-        u"\u2640-\u2642" 
-        u"\u2600-\u2B55"
-        u"\u200d"
-        u"\u23cf"
-        u"\u23e9"
-        u"\u231a"
-        u"\ufe0f"  # dingbats
-        u"\u3030"
-                      "]+", re.UNICODE)
-    return re.sub(emoj, '', data) 
+            u"\U0001F600-\U0001F64F"  # emoticons
+            u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+            u"\U0001F680-\U0001F6FF"  # transport & map symbols
+            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+            #u"\U00002702-\U000027B0"
+            #u"\U000024C2-\U0001F251"
+            "]+", flags=re.UNICODE)
+
+    return emoj.sub(r'', data) 
